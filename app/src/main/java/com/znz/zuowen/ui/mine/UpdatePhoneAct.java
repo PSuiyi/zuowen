faca22f653951a0b948d97c0f320c60da63e5098
panqiming <497555328@qq.com>
2017-10-18 17:58:38 +0800

    +好作文接口调用

--- app/src/main/java/com/znz/zuowen/ui/mine/UpdatePhoneAct.java
+++ app/src/main/java/com/znz/zuowen/ui/mine/UpdatePhoneAct.java
@@ -1,10 +1,19 @@
 package com.znz.zuowen.ui.mine;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.znz.compass.znzlibray.utils.StringUtil;
+import com.znz.compass.znzlibray.views.EditTextWithDel;
+import com.znz.compass.znzlibray.views.ZnzRemind;
+import com.znz.compass.znzlibray.views.ZnzToolBar;
 import com.znz.zuowen.R;
 import com.znz.zuowen.base.BaseAppActivity;
+import com.znz.zuowen.common.Constants;
 import com.znz.zuowen.ui.login.ResetPsdAct;
 
 import butterknife.Bind;
@@ -20,6 +29,16 @@ import butterknife.OnClick;
 public class UpdatePhoneAct extends BaseAppActivity {
     @Bind(R.id.tvSubmit)
     TextView tvSubmit;
+    @Bind(R.id.znzToolBar)
+    ZnzToolBar znzToolBar;
+    @Bind(R.id.znzRemind)
+    ZnzRemind znzRemind;
+    @Bind(R.id.llNetworkStatus)
+    LinearLayout llNetworkStatus;
+    @Bind(R.id.tvPhone)
+    TextView tvPhone;
+    @Bind(R.id.etPhoneNew)
+    EditTextWithDel etPhoneNew;
 
     @Override
     protected int[] getLayoutResource() {
@@ -33,12 +52,46 @@ public class UpdatePhoneAct extends BaseAppActivity {
 
     @Override
     protected void initializeNavigation() {
-        setTitleName("验证手机");
+        setTitleName("改绑手机号");
     }
 
     @Override
     protected void initializeView() {
+        if (!StringUtil.isBlank(mDataManager.readTempData(Constants.User.MOBILE))) {
+            mDataManager.setValueToView(tvPhone, "当前手机号：" + mDataManager.readTempData(Constants.User.MOBILE));
+        } else {
+            tvPhone.setVisibility(View.GONE);
+        }
 
+        etPhoneNew.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                handleBtnState();
+            }
+        });
+    }
+
+    /**
+     * 处理按钮状态
+     */
+    private void handleBtnState() {
+        if (StringUtil.isBlank(mDataManager.getValueFromView(etPhoneNew))) {
+            return;
+        }
+        if (!StringUtil.isMobile(mDataManager.getValueFromView(etPhoneNew))) {
+            return;
+        }
+        tvSubmit.setBackgroundResource(R.drawable.bg_btn_round);
     }
 
     @Override
@@ -55,6 +108,14 @@ public class UpdatePhoneAct extends BaseAppActivity {
 
     @OnClick(R.id.tvSubmit)
     public void onViewClicked() {
+        if (StringUtil.isBlank(mDataManager.getValueFromView(etPhoneNew))) {
+            mDataManager.showToast("请输入手机号");
+            return;
+        }
+        if (!StringUtil.isMobile(mDataManager.getValueFromView(etPhoneNew))) {
+            mDataManager.showToast("请输入正确的手机号");
+            return;
+        }
         gotoActivity(ResetPsdAct.class);
     }
 }
